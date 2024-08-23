06c44324a133b03366b091f2971ede90f9c26027
MartinH147 <martinh13@optusnet.com.au>
2024-08-23 18:44:57 +1000

    basic location and temp functionality

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
+// check out https://youtu.be/UjeXpct3p7M 
+
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useState } from 'react';
+import axios from 'axios';
+
 function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=bc33f02bf9c4eb2cebae8666c641e2b2`
+  
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input 
+      value={location} 
+      onChange={event => setLocation(event.target.value)}
+      onKeyPress={searchLocation}
+      placeholder='Enter Location'
+      type='text'
+      />
+      <h1>{data.name}</h1>
+      {data.main ? <h2>{data.main.temp}</h2> : null}
     </div>
   );
 }
